48697e5446597828dfd6e33624e2ea899e6d4718
Naftali Horviz <n0548498093@gmail.com>
2026-03-01 16:32:23 +0200

    build config & producer

---
+++ service_metadata/config.py
@@ -0,0 +1,13 @@
+import os, logging
+
+
+class Config:
+    def __init__(self, logger: logging.Logger):
+        self.logger = logger
+        try:
+            self.bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS", "localhost:9092")
+            self.topic = os.getenv("PRODUCER_TOPIC", "METADATA")
+            self.logger.info("environments: %s, %s", self.bootstrap_servers, self.topic)
+            
+        except:
+            self.logger.exception("error load env")
---
+++ service_metadata/producer.py
@@ -0,0 +1,41 @@
+import logging, json
+from confluent_kafka import Producer, KafkaException
+
+
+class Metadata_Producer:
+    def __init__(self, logger: logging.Logger, bootstrap_servers: dict, topic: str):
+        self.logger = logger
+        self.topic = topic
+        try:
+            self.producer = Producer({"bootstrap.servers": bootstrap_servers})
+            self.logger.info("producer | Kafka producer initialized")
+        except Exception:
+            self.logger.exception("producer | error creating producer")
+
+    def delivery_report(self, err, msg):
+        if err is not None:
+            self.logger.error(f"producer | delivery failed {err}")
+        else:
+            self.logger.debug(
+                f"✅ producer | Delivered to {msg.topic()} "
+                f"[{msg.partition()}] @ {msg.offset()}"
+            )
+
+    def publish(self, event: dict):
+        try:
+            self.producer.produce(
+                topic=self.topic, value=json.dumps(event), callback=self.delivery_report
+            )
+            self.producer.poll(0)
+            self.logger.info(f"✅ producer | send event")
+        except KafkaException as e:
+            self.logger.exception(f"producer | Kafka exception {e}")
+
+    def flush(self):
+        try:
+            self.producer.flush()
+            self.logger.debug("producer | flushed all messages")
+
+        except Exception:
+            self.logger.exception("producer | Kafka flush failed")
+            raise
